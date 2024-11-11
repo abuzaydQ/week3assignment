5c2249108bd8a29272da88bfa8a218ef5a944bd1
Jimoh Babatunde <qbabatunde@gmail.com>
2024-11-11 14:48:45 +0100

    Updated Week 3 Assignment

--- assigment3.py
+++ assigment3.py
@@ -16,17 +16,14 @@ def calculate_discount(price, discount_percent):
         return price
     
 
-try:
-    price =float(input("Enter the original price of the item: "))
-    discount_percent = float(input("Enter the discount percentage "))
+
+price =float(input("Enter the original price of the item: "))
+discount_percent = float(input("Enter the discount percentage "))
 
 
-    final_price = calculate_discount(price, discount_percent)
+final_price = calculate_discount(price, discount_percent)
 
-    if final_price < price:
-        print(f"The final price after applying the discount is: ${final_price:.2f}")
-    else:
-        print(f"No discount applied. The original price is: ${price:.2f}")
-
-except ValueError:
-    print("Please enter valid numbers for price and discount percentage.")
+if final_price < price:
+    print(f"The final price after applying the discount is: ${final_price:.2f}")
+else:
+    print(f"No discount applied. The original price is: ${price:.2f}")
